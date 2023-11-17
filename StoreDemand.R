855b68a1b5f94cd2a7926e6135540909377d3a01
Aaron Oustrich <aaron.oustrich@gmail.com>
2023-11-17 13:36:50 -0700

    Exponential Smoothing edits

--- StoreDemand.R
+++ StoreDemand.R
@@ -5,6 +5,8 @@ library(glmnet)
 library(parallel)
 library(ranger)
 library(parallel)
+library(timetk)
+library(modeltime)
 
 # Load data
 train <- vroom("data/train.csv")
@@ -18,12 +20,22 @@ subTrain <- train %>%
 # Recipe for time series data
 subRecipe <- recipe(sales ~ ., data = subTrain) %>% 
   step_date(date,features = c("year", "doy", "week", "quarter")) #%>%
-  # step_range(date_doy, min=0, max=pi) %>%
-  # step_mutate(sinDOY=sin(date_doy), cosDOY=cos(date_doy))
-
+# step_range(date_doy, min=0, max=pi) %>%
+# step_mutate(sinDOY=sin(date_doy), cosDOY=cos(date_doy))
 
 prepped <- prep(subRecipe)
 baked <- bake(prepped, new_data = subTrain)
+
+# New data with date features
+subTrain2 <- train %>%
+  filter(store == 7, item == 17) %>% 
+  select(-store, -item)
+
+subRecipe2 <- recipe(sales ~ ., data = subTrain2) %>% 
+  step_date(date,features = c("year", "doy", "week", "quarter"))
+
+prepped2 <- prep(subRecipe2)
+baked2 <- bake(prepped2, new_data = subTrain2)
 
 # # Create time series cross validation folds
 # subFolds <- sliding_period(subTrain, date, period = "1 year", 
@@ -72,7 +84,91 @@ collect_metrics(treeCVResults) %>%
   pull("mean")
   
 
+# Exponential Smoothing 1 -------------------------------------------------
+# Create cross validation split 
+cv_split <- time_series_split(subTrain, assess="3 months", cumulative = TRUE)
+
+es_model <- exp_smoothing() %>%
+  set_engine("ets") %>%
+  fit(sales~date, data=training(cv_split))
+
+## Cross-validate to tune model
+cv_results <- modeltime_calibrate(es_model,
+                                 new_data = testing(cv_split))
+## Visualize CV results (top row)
+p1 <- cv_results %>%
+  modeltime_forecast(
+    new_data = testing(cv_split),
+    actual_data = subTrain) %>%
+  plot_modeltime_forecast(.interactive=TRUE)
+
+## Evaluate the accuracy
+cv_results %>%
+  modeltime_accuracy() %>%
+  table_modeltime_accuracy(.interactive = FALSE)
+
+## Refit to all data then forecast
+es_fullfit <- cv_results %>%
+  modeltime_refit(data = subTrain)
+
+es_preds <- es_fullfit %>%
+  modeltime_forecast(h = "3 months") %>%
+  rename(date=.index, sales=.value) %>%
+  select(date, sales) %>%
+  full_join(., y=test, by="date") %>%
+  select(id, sales)
+
+p2 <- es_fullfit %>%
+  modeltime_forecast(h = "3 months", actual_data = subTrain) %>%
+  plot_modeltime_forecast(.interactive=FALSE)
 
 
 
 
+
+
+  
+
+
+
+
+
+
+# Exponential Smoothing 2 -------------------------------------------------
+cv_split2 <- time_series_split(subTrain2, assess="3 months", cumulative = TRUE)
+
+es_model2 <- exp_smoothing() %>%
+  set_engine("ets") %>%
+  fit(sales~date, data=training(cv_split2))
+
+## Cross-validate to tune model
+cv_results2 <- modeltime_calibrate(es_model2,
+                                  new_data = testing(cv_split2))
+## Visualize CV results (top row)
+p3 <- cv_results2 %>%
+  modeltime_forecast(
+    new_data = testing(cv_split2),
+    actual_data = subTrain2) %>%
+  plot_modeltime_forecast(.interactive=TRUE)
+
+## Evaluate the accuracy
+cv_results2 %>%
+  modeltime_accuracy() %>%
+  table_modeltime_accuracy(.interactive = FALSE)
+
+## Refit to all data then forecast
+es_fullfit2 <- cv_results2 %>%
+  modeltime_refit(data = subTrain2)
+
+es_preds2 <- es_fullfit2 %>%
+  modeltime_forecast(h = "3 months") %>%
+  rename(date=.index, sales=.value) %>%
+  select(date, sales) %>%
+  full_join(., y=test, by="date") %>%
+  select(id, sales)
+
+p4 <- es_fullfit2 %>%
+  modeltime_forecast(h = "3 months", actual_data = subTrain2) %>%
+  plot_modeltime_forecast(.interactive=FALSE)
+
+plotly::subplot(p1, p2, p3, p4, nrows=2)
